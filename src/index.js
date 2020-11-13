f872461df8c7adc6d1e6cf7d484550f8d018da3e
Isaac Contreras <iacontrerasg@icloud.com>
2020-11-13 16:01:46 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,7 +7,6 @@ const PORT = process.env.PORT || 8082;
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Headers");
   res.setHeader("Access-Control-Allow-Methods", "POST, GET");
   next();
 });
