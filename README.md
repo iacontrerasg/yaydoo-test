a000411a6d150d36d53d2cbd16cdd9dac7b05f9a
Isaac Contreras <iacontrerasg@icloud.com>
2020-11-13 18:23:29 -0600

    Add Readme & Swagger Docs

---
+++ README.md
@@ -0,0 +1,37 @@
+## Descripción
+
+Backend de prueba técnica yaydoo - Isaac Contreras
+
+## Instalación
+
+Instalar dependencias con yarn
+
+\$ yarn
+
+## Ejecución
+
+Ejecutar aplicación con yarn, por defecto se ejecutará en el puerto 8082.
+
+# Development
+
+\$ yarn dev
+
+## Endpoints
+
+La documentación también se puede consultar en {BASE_URL}/Docs (http://localhost:8082/docs) (Swagger)
+
+# GET v1/products
+
+Lista todos los Productos
+
+# GET /products/:category
+
+Lista todos los Productos de una categoría
+
+# POST /products/reload
+
+Vuelve a generar la BDD por medio del scraper
+
+# GET /categories
+
+Lista todas las categorías
--- src/index.js
+++ src/index.js
@@ -1,16 +1,20 @@
 const express = require("express");
 const { scrapeAmazon } = require("./services/scraperService");
 const { saveProducts } = require("./services/databaseService");
+const swaggerUi = require("swagger-ui-express");
 
 const app = express();
 const PORT = process.env.PORT || 8082;
+const swaggerDocument = require("../swagger.json");
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "POST, GET");
-  
+
   next();
 });
+
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.listen(PORT, async () => {
   console.log(`Up & Running on port: ${PORT}`);
